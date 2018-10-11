34ccd473c858c30365b052c761fde2983631efc7
liuhuichao <2207221755@qq.com>
2018-10-11 18:24:51 +0800

    修改模板方法测试代码

--- src/test/java/com/common/lib/demo/designpatterns/template/TemplateFactoryTest.java
+++ src/test/java/com/common/lib/demo/designpatterns/template/TemplateFactoryTest.java
@@ -26,13 +26,18 @@ public class TemplateFactoryTest {
 
     @Test
     public void getProcessTest() {
+        /**组装数据上下文**/
         ProcessContext processContext = new ProcessContext();
         Map<String, String> data = Maps.newHashMap();
         data.put("aa", "foo");
         processContext.setDemoDataMap(data);
         processContext.setProcessEnum(ProcessEnum.PROCESS_TYPE_ONE);
+
+        /**根据数据上下文中放置的标记来选择模板**/
         AbsProcessTemplate oneTemplate = processByEnumFactory.getProcessTemple(processContext.getProcessEnum());
         oneTemplate.processDataByType(processContext);
+
+        /**当增加新的类型时候，只需要继承模板抽象类，实现相应的处理方法即可，无需改动原业务代码,符合开放封闭的原则**/
 
     }
 }
