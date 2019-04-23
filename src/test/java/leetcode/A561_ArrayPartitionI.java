da870df1445f2f0aa9071d38cd04ce66bc3c44df
liuhuichao <2207221755@qq.com>
2019-04-23 17:45:09 +0800

    add Q344:实现 反转字符串(单个数组的双指针)

---
+++ src/test/java/leetcode/A561_ArrayPartitionI.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import org.junit.Test;
+
+/**
+ * \* Created: liuhuichao
+ * \* Date: 2019/4/23
+ * \* Time: 5:43 PM
+ * \* Description: 数组拆分 I
+ * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,4,3,2]
+ * <p>
+ * 输出: 4
+ * 解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+ * 提示:
+ * <p>
+ * n 是正整数,范围在 [1, 10000].
+ * 数组中的元素范围在 [-10000, 10000].
+ * \
+ */
+public class A561_ArrayPartitionI {
+
+    public int arrayPairSum(int[] nums) {
+        return -1;
+
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
