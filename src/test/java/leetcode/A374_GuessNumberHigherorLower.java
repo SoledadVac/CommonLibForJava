a125bc9d3355f74b9dba5ff5397c54dd029983ea
liuhuichao <2207221755@qq.com>
2019-04-30 10:49:04 +0800

    add Q374:add [猜数字大小]

--- src/test/java/leetcode/A374_GuessNumberHigherorLower.java
+++ src/test/java/leetcode/A374_GuessNumberHigherorLower.java
@@ -23,29 +23,25 @@ import org.junit.Test;
  */
 public class A374_GuessNumberHigherorLower {
     public int guessNumber(int n) {
-        int begin = 1;
-        int end = n;
-        while (begin <= end) {
-            int mid = (begin + end) / 2;
-            int guessResult = guess(mid);
-            if (guessResult == 0) {
+        int l = 0;
+        int r = n;
+        int mid;
+        if(guess(n) == 0) return n;
+        while(l <= r){
+            mid = l + (r - l) / 2;
+            if(guess(mid) == -1){
+                r = mid - 1;
+            }else if(guess(mid) == 1){
+                l = mid + 1;
+            }else{
                 return mid;
-            } else if (guessResult == 1) {
-                end = mid - 1;
-            } else {
-                begin = mid + 1;
             }
         }
-        return 0;
+        return l;
     }
 
     int guess(int num) {
-        if (num > 6) {
-            return 1;
-        }
-        if (num < 6) {
-            return -1;
-        }
+
         return 0;
     }
 
