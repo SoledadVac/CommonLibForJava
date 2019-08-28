a529191ea0ecf857f1f1be7412c5886c56d45978
liuhuichao <2207221755@qq.com>
2019-08-28 12:04:18 +0800

    add Q236:add [二叉树的最近公共祖先]

---
+++ src/test/java/leetcode/BinaryTree/A236_LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,86 @@
+package leetcode.BinaryTree;
+
+import org.junit.Test;
+
+/**
+ * \* Created: liuhuichao
+ * \* Date: 2019/8/27
+ * \* Time: 27:30 PM
+ * \* Description: 二叉树的最近公共祖先
+ * \
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ * 示例 1:
+ * <p>
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ * 示例 2:
+ * <p>
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出: 5
+ * 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ * <p>
+ * 说明:
+ * <p>
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉树中。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class A236_LowestCommonAncestorofaBinaryTree {
+
+
+    /**
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null && right == null) {
+            return null;
+        }
+        if (left == null) {
+            return right;
+        }
+        return left;
+    }
+
+
+    @Test
+    public void test() {
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n0 = new TreeNode(0);
+        TreeNode n8 = new TreeNode(8);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n4 = new TreeNode(4);
+        n3.left = n5;
+        n3.right = n1;
+        n5.left = n6;
+        n5.right = n2;
+        n2.left = n7;
+        n2.right = n4;
+        n1.left = n0;
+        n1.right = n8;
+        TreeNode n = lowestCommonAncestor(n3, n5, n1);
+        System.out.println(n.val);
+    }
+}
