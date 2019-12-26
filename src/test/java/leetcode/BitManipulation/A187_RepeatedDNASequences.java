fe896b3208b0434b3cf40333bbff61ca3ce6e1ad
liuhuichao <2207221755@qq.com>
2019-12-26 20:22:39 +0800

    add Q187:add [重复的DNA序列]

--- src/test/java/leetcode/BitManipulation/A187_RepeatedDNASequences.java
+++ src/test/java/leetcode/BitManipulation/A187_RepeatedDNASequences.java
@@ -1,10 +1,14 @@
 package leetcode.BitManipulation;
 
 import com.alibaba.fastjson.JSONObject;
+import com.kenai.jaffl.annotations.In;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Handler;
 
 /**
  * \* Created: liuhuichao
@@ -30,19 +34,36 @@ import java.util.List;
 public class A187_RepeatedDNASequences {
     public List<String> findRepeatedDnaSequences(String s) {
         List<String> result = new ArrayList<>();
-        if (s == null || s.length() < 1) {
+        if (s == null || s.length() < 10) {
             return result;
         }
-        //todo : 重复的DNA序列
-
-
-
+        Map<Character, Integer> table = new HashMap<>();
+        table.put('A', 0); //00
+        table.put('C', 1); //01
+        table.put('G', 2); //10
+        table.put('T', 3); //11
+        int[] subStringExist = new int[1 << 20];
+        int dataRange = 0;
+        int mask = ((1 << 18) - 1);
+        for (int i = 0; i < s.length(); i++) {
+            dataRange <<= 2; //原来积累的字母向左移动两位，方便低位加入新字母
+            dataRange += table.get(s.charAt(i)); //加入新的一个字母
+            if (i >= 9) {
+                // i 移动到10位之后
+                if (subStringExist[dataRange] == 1) {
+                    //如果之前加入过了，说明出现重复了
+                    result.add(s.substring(i - 9, i + 1));
+                }
+                subStringExist[dataRange] += 1;//标记又出现了一次
+                dataRange &= mask; //后18位保持，其他位置0
+            }
+        }
         return result;
     }
 
     @Test
     public void test() {
-        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"; //["AAAAACCCCC", "CCCCCAAAAA"]
         System.out.println(JSONObject.toJSONString(findRepeatedDnaSequences(s)));
     }
 }
