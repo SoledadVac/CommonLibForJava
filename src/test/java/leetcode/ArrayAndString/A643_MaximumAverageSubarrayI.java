c735d70805254c3090c52a0168afc490a0bcc6ed
liuhuichao <2207221755@qq.com>
2020-05-29 11:55:49 +0800

    add Q643:add [子数组最大平均数 I]

--- src/test/java/leetcode/ArrayAndString/A643_MaximumAverageSubarrayI.java
+++ src/test/java/leetcode/ArrayAndString/A643_MaximumAverageSubarrayI.java
@@ -1,5 +1,6 @@
 package leetcode.ArrayAndString;
 
+import org.apache.commons.collections.EnumerationUtils;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -28,7 +29,8 @@ import java.util.Arrays;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class A643_MaximumAverageSubarrayI {
-    public double findMaxAverage(int[] nums, int k) {
+    // 时间复杂度大，但是能通过
+    public double findMaxAverage0(int[] nums, int k) {
         if (nums.length == 1) {
             return nums[0];
         }
@@ -54,10 +56,61 @@ public class A643_MaximumAverageSubarrayI {
         return sum;
     }
 
+    /**
+     * 执行用时 :
+     * 6 ms
+     * , 在所有 Java 提交中击败了
+     * 33.71%
+     * 的用户
+     * 内存消耗 :
+     * 43.9 MB
+     * , 在所有 Java 提交中击败了
+     * 16.67%
+     * 的用户
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public double findMaxAverage1(int[] nums, int k) {
+        //构造 sum
+        double result;
+        int[] sum = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                sum[0] = nums[0];
+                continue;
+            }
+            sum[i] = nums[i] + sum[i - 1];
+        }
+        result = sum[k - 1];
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (i + k - 1 > nums.length - 1) {
+                break;
+            }
+            result = Math.max(result, sum[i + k - 1] - sum[i - 1]);
+        }
+        return result / k;
+    }
+
+
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0; //初始化为前K个数的和
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double result = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            result = Math.max(result, sum);
+        }
+        return result / k;
+    }
+
     @Test
     public void test() {
-        int[] nums = {8860, -853, 6534, 4477, -4589, 8646, -6155, -5577, -1656, -5779, -2619, -8604, -1358, -8009, 4983, 7063, 3104, -1560, 4080, 2763, 5616, -2375, 2848, 1394, -7173, -5225, -8244, -809, 8025, -4072, -4391, -9579, 1407, 6700, 2421, -6685, 5481, -1732, -8892, -6645, 3077, 3287, -4149, 8701, -4393, -9070, -1777, 2237, -3253, -506, -4931, -7366, -8132, 5406, -6300, -275, -1908, 67, 3569, 1433, -7262, -437, 8303, 4498, -379, 3054, -6285, 4203, 6908, 4433, 3077, 2288, 9733, -8067, 3007, 9725, 9669, 1362, -2561, -4225, 5442, -9006, -429, 160, -9234, -4444, 3586, -5711, -9506, -79, -4418, -4348, -5891};
-        int k = 93;
+        int[] nums = {0, 4, 0, 3, 2};
+        int k = 1;
         System.out.println(findMaxAverage(nums, k));
     }
 }
