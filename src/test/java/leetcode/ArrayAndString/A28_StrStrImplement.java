44c89e7f3cae18989f07f813af35ebc5e36980c3
liuhuichao <2207221755@qq.com>
2020-07-16 14:53:42 +0800

    add Q28:add [字符串查找-KMP算法]

--- src/test/java/leetcode/ArrayAndString/A28_StrStrImplement.java
+++ src/test/java/leetcode/ArrayAndString/A28_StrStrImplement.java
@@ -1,5 +1,6 @@
 package leetcode.ArrayAndString;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -103,9 +104,118 @@ public class A28_StrStrImplement {
     }
 
 
+    /**
+     * 获取最长公共前后缀数组
+     * <p>
+     * 例如对于字符串 abcba：
+     * <p>
+     * 前缀：它的前缀包括：a, ab, abc, abcb，不包括本身；
+     * 后缀：它的后缀包括：bcba, cba, ba, a，不包括本身；
+     * 最长公共前缀后缀：abcba 的前缀和后缀中只有 a 是公共部分，字符串 a 的长度为 1。
+     *
+     * @param needle
+     * @return
+     */
+    public int[] getCommonLong(String needle) {
+        int[] result = new int[needle.length()];
+        result[0] = -1;
+        for (int i = 1; i < needle.length(); i++) {
+            int index = i - 1;
+            if (index == 0) {
+                //一个字母时候，为0
+                result[i] = 0;
+                continue;
+            }
+            //大于等于两个时候
+            int begin = 0;
+
+
+           /* int begin = 0;
+            int end = index;
+
+            while (begin < index && end > 0 && begin < end) {
+                if (needle.charAt(begin) == needle.charAt(end)) {
+                    result[i]++;
+                }
+                begin++;
+                end--;
+            }*/
+        }
+        return result;
+    }
+
+    /**
+     * KMP
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr2(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        if ("".equals(haystack) && "".equals(needle)) {
+            return 0;
+        }
+        if (haystack.length() < 1) {
+            return -1;
+        }
+        if (needle.length() < 1) {
+            return 0;
+        }
+        int[] next = getNext(needle);
+        int i = 0;
+        int j = 0;
+        while (i < haystack.length() && j < needle.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                //回退
+                if (j == 0 || i == 0) {
+                    //第一个
+                    i++;
+                    j++;
+                }
+                j = next[j - 1];
+            }
+        }
+        return j == needle.length() ? i - j : -1;
+    }
+
+    /**
+     * 获取最长公共前后缀长度
+     *
+     * @param s
+     * @return
+     */
+    private int[] getNext(String s) {
+        int[] result = new int[s.length()];
+        int len = 0, i = 1;
+        while (i < s.length()) {
+            if (s.charAt(i) == s.charAt(len)) {
+                result[i++] = ++len;
+            } else if (len == 0) {
+                //匹配失败，匹配长度为 0，无公共串
+                result[i++] = 0;
+            } else {
+                //匹配失败，看看公共串有没有前缀和后缀相等的部分，有的话，相等部分的后一个字母比较
+                len = result[len - 1];
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void test0() {
+        String needle = "ACTGPACY"; // -1 ,0 ,0,0,0,0,1,2
+        System.out.println(JSONObject.toJSONString(getNext(needle)));
+    }
+
     @Test
     public void test() {
-        int result = strStr1("mississippi", "issip"); //4
+        int result = strStr2("mississippi", "issip"); //4
         System.out.println("result= " + result);
     }
 
